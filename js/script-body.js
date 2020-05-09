3c91ca930541fed5ec4fb84b7c032cca78e208f4
Fábio Martins <fabio.martins.01@hotmail.com>
2020-05-09 20:15:23 -0300

    alterações 09-mai-2020

---
+++ js/script-body.js
@@ -0,0 +1,16 @@
+/* 	
+	*** Modelo de arquivo de código Javascript
+	*** para inclusão no elemento BODY da página HTML.
+	*** Desenvolvido por Fábio de Almeida Martins.
+*/
+
+//document.write("Desenvolvido por Fábio de Almeida Martins. <br><br>© 2020  -  Fábio de Almeida Martins");
+
+/*
+var nome = prompt("Digite o seu nome: ");
+console.log("nome = " + nome);
+if (nome != null && nome != "") 
+	alert("Olá, " + nome + "! \nBem vindo à minha página Web!");
+*/
+
+//alert("Desenvolvido por Fábio Martins. \nRedirecionando para www.uol.com.br em 3 segundos...");
